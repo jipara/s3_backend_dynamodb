170d948a4e6126c3b70d7641949278f492b51958
jipara <50920745+jipara@users.noreply.github.com>
2023-03-28 18:42:48 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,3 +29,49 @@ resource "aws_dynamodb_table" "table_for_s3" {
     key_type       = "HASH"
   }
 }
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket-backend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetBucketVersioning"
+        Resource  = aws_s3_bucket.bucket-backend.arn
+      },
+      {
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:PutObject"
+        Resource  = "${aws_s3_bucket.bucket-backend.arn}/*"
+        Condition = {
+          StringEquals = {
+            "s3:ExistingObjectTag/LockID" = "false"
+          }
+        }
+      },
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = [
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:GetObject",
+          "s3:GetObjectVersion"
+        ]
+        Resource  = [
+          "${aws_s3_bucket.bucket-backend.arn}/*",
+          aws_s3_bucket.bucket-backend.arn
+        ]
+        Condition = {
+          StringEquals = {
+            "s3:ExistingObjectTag/LockID" = "true"
+          }
+        }
+      }
+    ]
+  })
+}
